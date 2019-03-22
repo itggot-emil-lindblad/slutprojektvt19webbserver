51b4604d83274cd620c0dfc676a7b6a68cf5c6a6
itggot-emil-lindblad <emil.lindblad@elev.ga.ntig.se>
2019-03-22 17:01:02 +0100

    Fixed error msg on wrong login

--- app.rb
+++ app.rb
@@ -6,15 +6,47 @@ require 'securerandom'
 require_relative 'database.rb'
 enable :sessions
 
+configure do
+    set :publicroutes, ["/"]
+end
+
+before do 
+    p "hej"
+    if settings.publicroutes.any?(request.path_info) == false
+        if session[:username] != nil
+            break
+        else
+            halt 401, 'Unauthorized Error 401'
+        end
+    end
+end
+
+helpers do
+    def set_error(msg)
+        session[:error] = msg
+    end
+    
+    def get_error()
+        error = session[:error]
+        session[:error] = nil
+        return error
+    end
+    
+    def error?
+        !session[:error].nil?
+    end
+end
+
 get('/') do
     slim(:login)
 end
 
 post('/login') do
+    p params["UserName"]
     if login(params) == true
         redirect('/dashboard')
     else
-        session[:wrong] = true
+        set_error("Fel användarnamn eller lösenord!")
         redirect('/')
     end
 end
@@ -22,3 +54,5 @@ end
 get('/dashboard') do
     slim(:index)
 end
+
+
--- database.rb
+++ database.rb
@@ -1,12 +1,14 @@
 def login(params)
+    p "hej"
     db = SQLite3::Database.new("db/data.db")
     db.results_as_hash = true
     result = db.execute("SELECT Id, UserName, Hash FROM users WHERE Username =?",params["UserName"])
+    p params
     if result == []
         return false
     elsif checkpassword(params["PassWord"],result[0]["Hash"]) == true
         session[:userid] = result[0]["Id"]
-        session[:username] = params["UserName"]
+        session[:username] = result[0]["UserName"]
         return true
     else
         return false
